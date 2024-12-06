b3a2e251990c352ef61584dc4c74d0341e57170e
Damandeep Singh <111175962+CraftyCodeSmith@users.noreply.github.com>
2024-12-06 16:48:55 +0530

    seeng how we can handle the interrupt of ois via sigint event

--- event.js
+++ event.js
@@ -1,30 +1,38 @@
-import { EventEmitter } from "events";
-class customevent extends EventEmitter {
-  constructor() {
-    super();
-  }
+// import { EventEmitter } from "events";
+// class customevent extends EventEmitter {
+//   constructor() {
+//     super();
+//   }
 
-  trigger() {
-    this.emit("event");
-    console.log("event emitted");
-  }
-}
+//   trigger() {
+//     this.emit("event");
+//     console.log("event emitted");
+//   }
+// }
 
-class catcher extends EventEmitter {
-  constructor() {
-    super();
-  }
+// class catcher extends EventEmitter {
+//   constructor() {
+//     super();
+//   }
 
-  catch(event) {
-    event.on("event", () => {
-      console.log("event triggered");
-    });
-  }
-}
+//   catch(event) {
+//     event.on("event", () => {
+//       console.log("event triggered");
+//     });
+//   }
+// }
 
-const event = new customevent();
-const catchEvent = new catcher();
+// const event = new customevent();
+// const catchEvent = new catcher();
 
-catchEvent.catch(event);
+// catchEvent.catch(event);
 
-event.trigger();
+// event.trigger();
+
+process.on("SIGINT", () => {
+  console.log("GOT YOU");
+  process.exit();
+});
+process.title = "Daman node script";
+console.log(process.title);
+setTimeout(() => {}, 100000);
